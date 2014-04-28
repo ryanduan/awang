61137fa6f7edc72f2ca30693cda7d093a3a00e9d
ryanduan <dyw564@gmail.com>
2014-04-29 00:23:33 +0800

    none

--- awang.py
+++ awang.py
@@ -3,11 +3,15 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        print self.request.uri
+        print self._transforms
         self.write("Hello, world")
+
+
 
 if __name__ == "__main__":
     application = tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/awang/?", MainHandler),
     ])
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
